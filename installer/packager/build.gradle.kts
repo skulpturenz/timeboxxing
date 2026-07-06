d4e39722b8bd94063a6335b676d44d0638f6dbe4
Naveen Mathew <55116576+nmathew98@users.noreply.github.com>
2026-07-06 21:11:28 +1200

    chore: mvp vibe slop - fix installer build

--- installer/packager/build.gradle.kts
+++ installer/packager/build.gradle.kts
@@ -25,6 +25,8 @@ dependencies {
     implementation(libs.kotlinx.serializationJson)
     implementation(libs.sentry)
     implementation(libs.jna)
+    implementation(libs.handlebars)
+    implementation(libs.playwright)
 
     implementation(libs.compose.uiToolingPreview)
 }
